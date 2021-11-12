01d37ffdd465942ff35e14e6c57436cb9a168a47
Luis Ballesteros <86980374+HermeticPinguica@users.noreply.github.com>
2021-11-12 16:15:01 -0600

    Update settings.py

--- github_actions/settings.py
+++ github_actions/settings.py
@@ -82,7 +82,7 @@ if getenv('GITHUB_WORKFLOW'):
             'USER': 'postgres',
             'PASSWORD': 'postgres',
             'HOST': 'localhost',
-            'PORT': '5433'
+            'PORT': '5432'
         }
     }
 else:
